f1009455e85190b15d928716098074ccc3516515
kenneth lee <kennethlee@kenneths-MBP.fios-router.home>
2023-01-24 11:58:58 -0500

    45.42

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,12 @@
-import { Text, View, SafeAreaView, Image } from "react-native";
+import { Text, View, SafeAreaView, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
   UserIcon,
   ChevronDownIcon,
-  SearchIcon,
+  MagnifyingGlassIcon,
   AdjustmentsIcon,
+  AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
@@ -18,17 +19,17 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5 ">
       <Text className="text-red-500">
         {/*Header*/}
-        <View className="flex-row pb-3 ote,s-center mx-4 space-x-2">
+        <View className="flex-row pb-3 ote,s-center mx-4 space-x-2 px-4">
           <Image
             source={{
               uri: "https://links.papareact.com/wru",
             }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
-          <View>
+          <View className="flex-1">
             <Text className="font-bold text-gray-400 text-xs">
               Deliver Now!
             </Text>
@@ -38,6 +39,17 @@ const HomeScreen = () => {
             </Text>
           </View>
           <UserIcon size={35} color="#00CCBB" />
+        </View>
+        {/* Search */}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+            <MagnifyingGlassIcon color="gray" size={20} />
+            <TextInput
+              placeholder="Restaurants and cuisines"
+              keyboardType="default"
+            />
+          </View>
+          <AdjustmentsVerticalIcon color="#00CCBB" size={30} />
         </View>
       </Text>
     </SafeAreaView>
